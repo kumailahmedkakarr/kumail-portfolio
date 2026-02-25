12753b93e410d33adbad2b069a785f51a583f36f
Kumail Ahmed <kumailahmedofficial@gmail.com>
2026-02-25 23:52:10 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav ul li a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 200) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(a => {
+    a.classList.remove("active");
+    if (a.getAttribute("href").includes(current)) {
+      a.classList.add("active");
+    }
+  });
+});
